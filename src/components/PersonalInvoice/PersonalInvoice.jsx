0aea3f29bb76f97394d6026a0905527268d16d87
rust_man <gasymovrustam86@yandex.com>
2021-11-29 05:49:13 +0200

    creating structure of personalInfo page

---
+++ src/components/PersonalInvoice/PersonalInvoice.jsx
@@ -0,0 +1,9 @@
+import styles from "./PersonalInvoice.module.css";
+
+export default function PersonalInvoice({ open, setOpen, id }) {
+  return (
+    <section className={styles.container}>
+      <div className={wrapper}></div>
+    </section>
+  );
+}
